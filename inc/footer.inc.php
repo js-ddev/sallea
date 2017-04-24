4e09ceae392db3aa3afdb18549c3f574f1965c00
js-ddev <jsd_cnd@yahoo.fr>
2017-04-24 10:57:02 +0200

    deuxieme commit

---
+++ inc/footer.inc.php
@@ -0,0 +1,9 @@
+</div>
+</section>
+<footer>
+    <div class="conteneur">
+        <?= date ('Y')?> - Tous droits reservés.
+    </div>
+</footer>
+</body>
+</html>
---
+++ inc/init.inc.php
@@ -0,0 +1,23 @@
+<?php
+
+$pdo = new PDO('mysql:host=localhost;dbname=site','root','',
+array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING,
+PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
+
+session_start();
+
+// VARIABLES
+$msg='';
+$page= '';
+$contenu ='';
+
+// AUTRES INCLUSIONS
+require_once('fonctions.inc.php');
+
+// CHEMIN : on va créer une constante racine site pour gérer les redrections dans le site:
+define('RACINE_SITE','/site/');
+
+// CHEMIN : on va créer une constante racine serveur pour le déplacement des fichiers :
+define('RACINE_SERVEUR',$_SERVER['DOCUMENT_ROOT']);
+
+ ?>
